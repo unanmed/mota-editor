547b2717ef305e822d0388f20dacfaadbfea5588
unanmed <1319491857@qq.com>
2023-03-09 12:49:20 +0800

    可折叠表格

--- src/panel/view/components/table.tsx
+++ src/panel/view/components/table.tsx
@@ -1,6 +1,7 @@
 import { ref } from 'vue';
 import { RightOutlined } from '@ant-design/icons-vue';
 import { Button } from 'ant-design-vue';
+import Table from './table.vue';
 
 export interface TableElement {
     type: 'object' | 'code' | 'text';
@@ -22,47 +23,7 @@ export function TableRenderer(props: TableProps) {
     const data = props.data;
 
     if (data.type === 'object') {
-        const toRender: Record<string, TableElement> = {};
-        for (const [key, d] of Object.entries(data)) {
-            if (typeof d !== 'string') toRender[key] = d;
-        }
-        const opened = ref(true);
-
-        return (
-            <div class={'table-root'}>
-                <div
-                    class={'table-folder'}
-                    onClick={() => (opened.value = !opened.value)}
-                    style={{
-                        backgroundColor: `#${(3 + props.n)
-                            .toString()
-                            .repeat(3)}`
-                    }}
-                >
-                    <RightOutlined
-                        class={'table-fold-icon'}
-                        style={{
-                            transition: `transform 0.3s ease`,
-                            transform: opened.value ? `rotate(90deg)` : `none`
-                        }}
-                    />
-                    <span class={'table-fold-key'}>{data.text}</span>
-                </div>
-                {opened.value && (
-                    <div class={'table-content'}>
-                        {Object.entries(toRender).map(v => {
-                            return (
-                                <TableRenderer
-                                    keys={v[0]}
-                                    data={v[1]}
-                                    n={props.n + 1}
-                                ></TableRenderer>
-                            );
-                        })}
-                    </div>
-                )}
-            </div>
-        );
+        return <Table keys={props.keys} data={data} n={props.n}></Table>;
     } else {
         return (
             <div class={'table-one'}>
--- src/panel/view/components/table.vue
+++ src/panel/view/components/table.vue
@@ -1,5 +1,49 @@
-<template></template>
+<template>
+    <div class="table-root">
+        <div
+            class="table-folder"
+            @click="() => (opened = !opened)"
+            :style="{
+                backgroundColor: `#${(3 + props.n).toString().repeat(3)}`
+            }"
+        >
+            <RightOutlined
+                class="table-fold-icon"
+                :style="{
+                    transition: `transform 0.3s ease`,
+                    transform: opened ? `rotate(90deg)` : `none`
+                }"
+            />
+            <span class="table-fold-key">{{ data.text }}</span>
+        </div>
+        <div v-if="opened" class="table-content">
+            <TableRenderer
+                v-for="(data, key) of toRender"
+                :keys="key"
+                :data="data"
+                :n="n + 1"
+            ></TableRenderer>
+        </div>
+    </div>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { TableElement, TableRenderer } from './table';
+import { RightOutlined } from '@ant-design/icons-vue';
+
+const props = defineProps<{
+    keys: string;
+    data: TableElement;
+    n: number;
+}>();
+
+const opened = ref(true);
+
+const toRender: Record<string, TableElement> = {};
+for (const [key, d] of Object.entries(props.data)) {
+    if (typeof d !== 'string') toRender[key] = d;
+}
+</script>
 
 <style lang="less" scoped></style>
